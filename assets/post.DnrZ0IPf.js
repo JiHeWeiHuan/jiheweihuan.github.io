d88bc152165f942cd403f081262a6c5b378e6c80
JiHeWeiHuan <3025782865@qq.com>
2025-10-08 19:17:41 +0800

    20251008-v2

---
+++ assets/post.DnrZ0IPf.js
@@ -0,0 +1,1 @@
+import"./chunks/@vueuse/motion.DpTphudp.js";import{p as t}from"./app.c4ZJ5Pxz.js";import{R as e}from"./framework.BIuMpVrY.js";function m(o){if(!o)return{color:"",icon:"",styles:void 0};const r=t();o in r.value.types||(o="link");const s=r.value.types[o].color,i=r.value.types[o].icon,n=e(()=>o?{"--card-c-primary":s}:void 0);return{color:s,icon:i,styles:n}}export{m as u};
