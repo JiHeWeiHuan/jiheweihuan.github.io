ff532c617decbd498ddfe2cedd9317c7a69d2504
JiHeWeiHuan <3025782865@qq.com>
2025-10-09 23:05:47 +0800

    v2.2.0-20251009

--- assets/collections.DQ7bgkiU.js
+++
@@ -1,1 +0,0 @@
-import"./chunks/@vueuse/motion.DeZQgAl7.js";import{u as l}from"./chunks/vue-router.C2BPWMrR.js";import{R as t}from"./framework.DjuiFVcZ.js";const e=[];function a(){return{collections:t(()=>e)}}function p(){const o=l(),n=t(()=>o.path.startsWith("/collections/")?o.path.split("/")[2]:"");return{collection:t(()=>e.find(c=>c.key===n.value))}}export{a,p as u};
--- assets/index.CFuZbgTV.js
+++
@@ -1,1 +0,0 @@
-import"./chunks/@vueuse/motion.DeZQgAl7.js";import{B as s}from"./app.v08uiZ1W.js";import"./chunks/dayjs.BA9YVBT0.js";import"./animation.BAUldMwQ.js";const m="/assets/none.DLM1AMsD.jpg";function p(t,o=m){const r=t.target;r.setAttribute("data-src",r.src),r.src=o}function c(t){return s(t,{template:"YYYY-MM-DD HH:mm:ss"})}export{c as f,p as o};
